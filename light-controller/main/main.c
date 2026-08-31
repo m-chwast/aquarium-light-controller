2126951d8c4e1f726cea9abfec0d73aa5b6b0e4c
Mateusz Chwast <chwastmat@gmail.com>
2026-08-31 22:22:27 +0200

    add lcd task

--- light-controller/main/main.c
+++ light-controller/main/main.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
+
+#include "driver/gpio.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_ops.h"
+#include "esp_lcd_panel_vendor.h"
+#include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
 
 #define BLINK_GPIO GPIO_NUM_8
 
-void app_main(void)
-{
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+static void blink_task(void* arg);
+static void lcd_task(void* arg);
 
-    while (1) {
-        gpio_set_level(BLINK_GPIO, 1);
-        vTaskDelay(pdMS_TO_TICKS(1500));
+void app_main(void) {
+	xTaskCreate(&blink_task, "BLINK", 1024, NULL, 5, NULL);
+	xTaskCreate(&lcd_task, "LCD", 2048, NULL, 5, NULL);
+}
 
-        gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(pdMS_TO_TICKS(500));
-    }
+static void lcd_task(void* arg) {
+	while(1) {
+		ESP_LOGI("LCD", "Hello, LCD!");
+		vTaskDelay(pdMS_TO_TICKS(1000));
+	}
+}
+
+static void blink_task(void* arg) {
+	gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+
+	while(1) {
+		gpio_set_level(BLINK_GPIO, 1);
+		vTaskDelay(pdMS_TO_TICKS(1000));
+
+		gpio_set_level(BLINK_GPIO, 0);
+		vTaskDelay(pdMS_TO_TICKS(1000));
+	}
 }
