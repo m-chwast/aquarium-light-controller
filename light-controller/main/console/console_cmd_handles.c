a85d1cd2f3b9d6e637e3d8703cda577740bd8def
Mateusz Chwast <chwastmat@gmail.com>
2026-09-06 01:21:17 +0200

    send calibration request

--- light-controller/main/console/console_cmd_handles.c
+++ light-controller/main/console/console_cmd_handles.c
@@ -1,9 +1,11 @@
 #include "console_cmd_handles.h"
 
+#include "display/display_request.h"
 #include "esp_log.h"
 
 #define CONSOLE_CMD_TAG "CONSOLE_CMD"
 
 void console_cmd_handles_calib(void) {
 	ESP_LOGI(CONSOLE_CMD_TAG, "Display calibration command received");
+	display_request_send(DISPLAY_REQUEST_TYPE_CALIBRATION);
 }
