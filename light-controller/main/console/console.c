bc5dbf99ee383c8c32b84b6c0408e60e8b2d18a8
Mateusz Chwast <chwastmat@gmail.com>
2026-09-06 00:59:15 +0200

    extract lines

--- light-controller/main/console/console.c
+++ light-controller/main/console/console.c
@@ -9,13 +9,15 @@
 
 #define CONSOLE_BUFFER_SIZE 256
 
+#define CONSOLE_TERMINATION_CHAR '\n'
+
 static void console_task_handler(void* arg);
 
 typedef struct console_t {
 	rtos_task_t task;
 
 	rtos_queue_t rx_buffer;
-	bool ready_to_parse;
+	bool ready_to_process;
 	bool rx_overflow;
 } console_t;
 
@@ -35,7 +37,7 @@ void console_init(void) {
 
 static void console_append_char(char c) {
 	if(c == '\r') {
-		c = '\n';
+		c = CONSOLE_TERMINATION_CHAR;
 	}
 
 	const unsigned free_space = rtos_queue_get_free_space(console.rx_buffer);
@@ -47,9 +49,27 @@ static void console_append_char(char c) {
 
 	rtos_queue_send(console.rx_buffer, &c);
 
-	if(c == '\n') {
-		console.ready_to_parse = true;
+	if(c == CONSOLE_TERMINATION_CHAR) {
+		console.ready_to_process = true;
 	}
+}
+
+static void console_process(void) {
+	// this function assumes that buffer contains '\n' character
+	char line[CONSOLE_BUFFER_SIZE] = {0};
+
+	char c = '\0';
+	unsigned i = 0;
+	while(c != CONSOLE_TERMINATION_CHAR) {
+		rtos_queue_receive(console.rx_buffer, &c);
+		line[i] = c;
+		i++;
+	}
+
+	// remove the termination character
+	line[i - 1] = '\0';
+
+    ESP_LOGI(CONSOLE_TAG, "Received line: %s", line);
 }
 
 static void console_task_handler(void* arg) {
@@ -61,9 +81,12 @@ static void console_task_handler(void* arg) {
 			const char c = (char)new_data;
 			console_append_char(c);
 
-			ESP_LOGI(CONSOLE_TAG, "Received char: %c", c);
-
 			new_data = getchar();
+		}
+
+		if(console.ready_to_process) {
+			console.ready_to_process = false;
+			console_process();
 		}
 	}
 }
