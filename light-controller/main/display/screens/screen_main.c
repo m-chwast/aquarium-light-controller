06d02a8fc4324b3d8e7876f43bce48dae87f08e9
Mateusz Chwast <chwastmat@gmail.com>
2026-09-10 23:39:26 +0200

    add button with ble scan request

--- light-controller/main/display/screens/screen_main.c
+++ light-controller/main/display/screens/screen_main.c
@@ -1,11 +1,17 @@
 #include "screen_main.h"
 
+#include "bt/bt_request.h"
+
 typedef struct screen_main_t {
 	lv_obj_t* screen;
 	lv_obj_t* label;
+
+	lv_obj_t* btn_scan;
 } screen_main_t;
 
 static screen_main_t screen_main;
+
+static void screen_main_event_handler(lv_event_t* event);
 
 screen_handle_t screen_main_init(void) {
 	lvgl_port_lock(0);
@@ -20,48 +26,46 @@ screen_handle_t screen_main_init(void) {
 	lv_obj_set_style_text_color(label, lv_color_white(), 0);
 	lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 
-	lv_obj_t* btn = lv_btn_create(screen);
+	lv_obj_t* btn1 = lv_btn_create(screen);
 
 	// button top
-	lv_obj_set_size(btn, 120, 60);
-	lv_obj_align_to(btn, NULL, LV_ALIGN_TOP_MID, 0, 20);
+	lv_obj_set_size(btn1, 120, 60);
+	lv_obj_align_to(btn1, NULL, LV_ALIGN_TOP_MID, 0, 20);
 
-	lv_obj_set_style_bg_color(btn, lv_color_hex(0x00FF00), LV_PART_MAIN);
-	lv_obj_set_style_bg_opa(btn, LV_OPA_COVER, LV_PART_MAIN);
+	lv_obj_set_style_bg_color(btn1, lv_palette_main(LV_PALETTE_AMBER),
+							  LV_PART_MAIN);
+	lv_obj_set_style_bg_opa(btn1, LV_OPA_COVER, LV_PART_MAIN);
 
-	lv_obj_t* label1 = lv_label_create(btn);
-	lv_label_set_text(label1, "GREEN");
+	lv_obj_t* label1 = lv_label_create(btn1);
+	lv_label_set_text(label1, "BLE Scan");
 	lv_obj_center(label1);
 
-	// button middle
-	lv_obj_t* btn2 = lv_btn_create(screen);
-	lv_obj_set_size(btn2, 120, 60);
-	lv_obj_center(btn2);
-
-	lv_obj_set_style_bg_color(btn2, lv_color_hex(0xFF0000), LV_PART_MAIN);
-	lv_obj_set_style_bg_opa(btn2, LV_OPA_COVER, LV_PART_MAIN);
-
-	lv_obj_t* label2 = lv_label_create(btn2);
-	lv_label_set_text(label2, "RED");
-	lv_obj_center(label2);
-
-	// button bottom
-	lv_obj_t* btn3 = lv_btn_create(screen);
-	lv_obj_set_size(btn3, 120, 60);
-	lv_obj_align_to(btn3, NULL, LV_ALIGN_BOTTOM_MID, 0, -20);
-
-	lv_obj_set_style_bg_color(btn3, lv_color_hex(0x0000FF), LV_PART_MAIN);
-	lv_obj_set_style_bg_opa(btn3, LV_OPA_COVER, LV_PART_MAIN);
-
-	lv_obj_t* label3 = lv_label_create(btn3);
-	lv_label_set_text(label3, "BLUE");
-	lv_obj_center(label3);
+	lv_obj_add_event_cb(btn1, screen_main_event_handler, LV_EVENT_ALL,
+						NULL);
 
 	lvgl_port_unlock();
 
 	screen_main.screen = screen;
 	screen_main.label = label;
+	screen_main.btn_scan = btn1;
 
 	const screen_handle_t handle = {.screen = screen_main.screen};
 	return handle;
+}
+
+static void screen_main_event_handler(lv_event_t* event) {
+	const lv_event_code_t code = lv_event_get_code(event);
+	const lv_obj_t* target = lv_event_get_target(event);
+
+	switch(code) {
+		case LV_EVENT_CLICKED: {
+			if(target == screen_main.btn_scan) {
+				bt_request_send(BT_REQUEST_TYPE_START_SCAN);
+			}
+			break;
+		}
+		default: {
+			break;
+		}
+	}
 }
